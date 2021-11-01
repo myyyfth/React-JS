d75cafb36b72c04cd4277d2baf998a9f3da61f2e
Ahmad Miftah Syakir <ahmadmiftahsyakir@192.168.1.5>
2021-11-01 07:40:10 +0700

    berhasil di push

--- scope.js
+++ scope.js
@@ -1,82 +1,81 @@
+// var a = "ahmad" // global scope
+// var b = "miftah"
+// var c = "syakir"
+// var d = "mas miftah"
+// var saya = "ahmad mifath syakir"
+// var gw = "syakir ramadhan"
 
-var a = "ahmad" // global scope
-var b = "miftah"
-var c = "syakir"
-var d = "mas miftah"
-var saya = "ahmad mifath syakir"
-var gw = "syakir ramadhan"
+// function alert() {
+//     console.log(a, b, c)
+// }
+// alert()
 
-function alert() {
-    console.log(a, b, c)
-}
-alert()
+// function print(param) {
+//     console.log(param)
+// }
+// print(c)
 
-function print(param) {
-    console.log(param)
-}
-print(c)
+// //pembatas//
 
-//pembatas//
+// function alert() {
+//     console.log(d)
+// }
+// alert()
 
-function alert() {
-    console.log(d)
-}
-alert()
+// function print(param){
+// console.log(param)
+// }
+// print(d)
 
-function print(param){
-console.log(param)
-}
-print(d)
+// //pembatas//
 
-//pembatas//
+// const cetak1 = function () {
+//     console.log(saya)
+// }
+// const cetak5 = function () {
+//     console.log(gw)
+// }
+// var saya = "ganteng abiss"
+// const cetak2 = function () {
+//     console.log(saya)
+// }
+// var saya = "Biasa aja -_-"
+// const cetak3 = function () {
+//     console.log(saya)
+// }
+// var gw = "Beuhhh demage nya >///<"
+// const cetak6 = function() {
+//     console.log(gw)
+// }
 
-const cetak1 = function () {
-    console.log(saya)
-}
-const cetak5 = function () {
-    console.log(gw)
-}
-var saya = "ganteng abiss"
-const cetak2 = function () {
-    console.log(saya)
-}
-var saya = "Biasa aja -_-"
-const cetak3 = function () {
-    console.log(saya)
-}
-var gw = "Beuhhh demage nya >///<"
-const cetak6 = function() {
-    console.log(gw)
-}
+// //pembatas//
 
-//pembatas//
+// const hello = new String("hallo")
+// hello.nama = "ngab"
+// hello.print = function(){
+//     console.log(this+ " " + this.nama)
+// }
 
-const hello = new String("hallo")
-hello.nama = "ngab"
-hello.print = function(){
-    console.log(this+ " " + this.nama)
-}
+// const yellow = new String("miftah")
+// yellow.name = " pinter bgt njirr"
+// yellow.print = function() {
+//     console.log(this + " asli" + this.name + this.nama)
+// }
+// yellow.print()
+// hello.print()
+// cetak1()
+// cetak2()
+// cetak3()
+// cetak5()
+// cetak6()
 
-const yellow = new String("miftah")
-yellow.name = " pinter bgt njirr"
-yellow.print = function() {
-    console.log(this + " asli" + this.name + this.nama)
-}
-yellow.print()
-hello.print()
-cetak1()
-cetak2()
-cetak3()
-cetak5()
-cetak6()
+// //pembatas//
+// function tampil(){
+//     form =  prompt("Siapa Nama Antum : ","");
+//     document.write("SUp" +form+ ",guys");
+//     }
 
-//pembatas//
-function tampil(){
-    form =  prompt("Siapa Nama Antum : ","");
-    document.write("SUp" +form+ ",guys");
-    }
-
-    //pembatas//
+//     //pembatas//
     let a = 2
     const kuadrat = function(num) {
         let retult = num * num
@@ -94,6 +93,44 @@ const d = 'iya'
 const e = 'iya'
 const s = 'iya'
 const r = 'iya'
-console.log('${a} ${b}')
+console.log('${a}perkenalkan ${b}')
 
 console.log(a + '' + b)
+
+//pembatas//
+let a = 1
+let b = 2
+let c = 3
+console.log("Nilai a adalah" +a+ ", Nilai b adalah" +b+ ", Nilai c adalah" + c)
+
+function iniGenap(num){
+    return num %2 == 0
+}
+
+//pembatas//
+function hasil(num){
+    const benar = iniGenap(num)
+    if(benar){
+        console.log('${num} adalah genap')
+    }else{
+        console.log('{num} adalah ganjil')
+    }
+}
+
+hasil(7, Genap)
+
+//pembatas//
+// function genap(num1) {
+//     return num % 2 == 0
+// }
+
+// function cetak(num, callback) {
+//     const benar = callback(num)
+//     if (benar) {
+//         console.log('${num} adalah bilangan genap')
+//     } else {
+//         console.log('${num} adalah bilangan ganjil')
+//     }
+// }
+
+// cetak(17, (angka => angka%2 == 0))
